72a0b5a45b555063653b1abdab817cfdfc440877
Reyhan Dwiyan <rereeaa56@gmail.com>
2024-05-07 12:22:08 +0700

    view

--- public/view/tambah.php
+++ public/view/tambah.php
@@ -35,29 +35,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Siswa</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </head>
 <body>
-    <div>
-        <h3>Tambah Data Siswa</h3>
-        <form method="POST">
-            <div>
-                <label>No.Kartu</label>
-                <input type="text" name="nokartu" id="nokartu" placeholder="Nomor Kartu RFID" class="form-control">
-            </div>
-            <div>
-                <label>NISN</label>
-                <input type="text" name="nisn" id="nisn" placeholder="NISN" class="form-control">
-            </div>
-            <div>
-                <label>Nama</label>
-                <input type="text" name="nama" id="nama" placeholder="Nama Siswa" class="form-control">
-            </div>
-            <div>
-                <label>Kelas</label>
-                <input type="text" name="Kelas" id="kelas" placeholder="Kelas" class="form-control">
-            </div>
-            <button name="btnSimpan" id="btnSimpan">Simpan</button>
-        </form>
+    <?php include 'components/sidebar.php'?>
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+            <h3><b>Tambah Data Siswa</b></h3>
+            <form method="POST">
+                <div>
+                    <label>No.Kartu</label>
+                    <input type="text" name="nokartu" id="nokartu" placeholder="Nomor Kartu RFID" class="form-control">
+                </div>
+                <div>
+                    <label>NISN</label>
+                    <input type="text" name="nisn" id="nisn" placeholder="NISN" class="form-control">
+                </div>
+                <div>
+                    <label>Nama</label>
+                    <input type="text" name="nama" id="nama" placeholder="Nama Siswa" class="form-control">
+                </div>
+                <div>
+                    <label>Kelas</label>
+                    <input type="text" name="kelas" id="kelas" placeholder="Kelas" class="form-control">
+                </div>
+                <button name="btnSimpan" id="btnSimpan">Simpan</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
