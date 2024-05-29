df4ee608d08f1acefe567e706b31769bb446a301
Reyhan Dwiyan <rereeaa56@gmail.com>
2024-05-29 11:51:53 +0700

    tambahan tampilan dashboard

--- public/view/dashboard.php
+++ public/view/dashboard.php
@@ -14,8 +14,29 @@
 
     <!-- Content -->
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-            <h1>INI DASHBOARD</h1>
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">    
+            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jumlah Siswa</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <div class="mb-4">
+                        <?php
+                            include "koneksi.php";
+                            $count_query = mysqli_query($koneksi, "SELECT COUNT(*) as total_siswa FROM db_siswa");
+                            $count_result = mysqli_fetch_assoc($count_query);
+                            $total_siswa = $count_result['total_siswa'];
+                        ?>
+                        <p class="text-lg font-medium">Jumlah Siswa: <?php echo $total_siswa; ?></p>
+                    </div>
+                </p>
+                <a href="siswa.php" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+            </div>
         </div>
     </div>
 </body>
